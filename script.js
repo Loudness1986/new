761d8283d43cf09578f11a851dbde8aedad62181
Loudness1986 <jacinto2014@gmail.com>
2022-01-23 04:08:35 -0400

    coordinates
    update

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 9.565446,
+                lng: -69211871,
             },
         },
     ];
